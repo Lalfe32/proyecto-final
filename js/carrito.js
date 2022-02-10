d0a1a174f46c6224e3ecbfec1d1af714deae4243
Leandro Lopez <lopezlh982@gmail.com>
2022-02-10 02:43:06 -0300

    armando el carrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,11 +1,20 @@
-class carrito{
+const addCartButton = document.querySelectorAll('.add-cart');
 
-    //Añadir el producto al carrito
-    compraProducto(e){
-        e.preventDefault();
-        if(e.target.classlist.contains('add-cart')){
-            const producto = e.target.parentElement.parentElement;
-            this.leerDatosProducto(producto);
-        }
-    }
+addCartButton.forEach(addToCartButton => {
+    addToCartButton.addEventListener('click', addToCartClicked);
+});
+
+function addToCartClicked (event){
+    const button = event.target;
+    const card = button.closest('.card');
+
+    const cardTitle = card.querySelector('.card-title').textContent;
+    const cardPrice = card.querySelector('.card-price').textContent;
+    const cardImg = card.querySelector('.card-image').src;
+
+    addCardToShoppingCart(cardTitle, cardPrice, cardImg)
+}
+
+function addCardToShoppingCart(cardTitle, cardPrice, cardImg) {
+    console.log(cardTitle, cardPrice, cardImg)
 }
